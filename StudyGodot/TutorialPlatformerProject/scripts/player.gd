64da70432acd020e7500748111a4eb2fbf6e661d
Igor Simões <igorsimoes11.06@gmail.com>
2025-02-18 00:28:29 +0000

    Improve player sprite and animation when moving or jumping

--- StudyGodot/TutorialPlatformerProject/scripts/player.gd
+++ StudyGodot/TutorialPlatformerProject/scripts/player.gd
@@ -4,19 +4,38 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	var moving_right := direction > 0
+	var moving_left := direction < 0
+	var idle := direction == 0
+	
+	# Flip the Sprite
+	if moving_right:
+		animated_sprite.flip_h = false
+	elif moving_left:
+		animated_sprite.flip_h = true
+	
+	# Play animations
+	if is_on_floor():	
+		# Handle jump
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = JUMP_VELOCITY
+			animated_sprite.play("jumping") 
+		elif idle:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("running")
+	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
